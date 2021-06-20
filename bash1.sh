d20191986f56d20abf1e8b7c482715af71061715
jyancan <jyancan@gmail.com>
2021-06-20 00:57:09 +0000

    update yaml files

--- bash1.sh
+++ bash1.sh
@@ -3,4 +3,6 @@ sagcp1=gcpuser1
 sagcp2="$(gcloud iam service-accounts create $gcpuser1)"
 iam="$(gcloud iam service-accounts list --filter "${sagcp2}")"
 key="$(gcloud iam service-accounts keys create "${sagcp1}".json --iam-account "${iam}")"
-
+terraform init
+terraform plan -auto-approve
+terraform apply -var='google_credentials="${sagcp1}".json"'
--- docker.sh
+++ docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#cerando imagen
+#creando imagen
 name=imageAPI
 cd container
 docker build -t $name .
-imageID=(docker images | grep $nam | awk '{print $3}'  )
+imageID="$(docker images | grep ${name} | awk '{print $3}'  )"
 #docker run -ti -p 8080:3000 $imageID
 
 #subiedo imagen a gcr.io
